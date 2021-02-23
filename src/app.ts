952c25a6077a8fd8526e1dd9e8a6a410e2a90c6a
Ankit Kumar Pandit <ankitp@geekyants.com>
2021-02-23 16:59:48 +0530

    🎉 Project created

---
+++ src/app.ts
@@ -0,0 +1,9 @@
+import express, { Application, Request, Response } from 'express';
+
+const app: Application = express();
+
+app.get('/', (_: Request, res: Response) => {
+  res.send("Hello");
+});
+
+app.listen(2108, () => console.log("Staring at 2108"))
