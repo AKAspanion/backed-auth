dd87f8f520e8682298f9bce9fac61604c7d0b439
Ankit Kumar Pandit <ankitp@geekyants.com>
2021-02-23 17:18:10 +0530

    🎨 Prettier linting added

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import express, { Application, Request, Response } from 'express';
 const app: Application = express();
 
 app.get('/', (_: Request, res: Response) => {
-  res.send("Hello");
+  res.send('Hello');
 });
 
-app.listen(2108, () => console.log("Staring at 2108"))
+app.listen(2108, () => console.log('Staring at 2108'));
